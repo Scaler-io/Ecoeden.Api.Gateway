9245c97c47e522f71c288fa7061e2c2fbda90d54
sharthak1095 <sharthakmallik@gmail.com>
2024-11-24 23:50:30 +0530

    bff api onboard

--- src/Ecoeden.Api.Gateway/Services/Subscription/SubscriptionValidationService.cs
+++ src/Ecoeden.Api.Gateway/Services/Subscription/SubscriptionValidationService.cs
@@ -7,11 +7,12 @@ public class SubscriptionValidationService(ILogger logger) : ISubscriptionValida
 {
     private readonly ILogger _logger = logger;
 
-    private readonly Dictionary<string, List<string>> _subscriptions = new Dictionary<string, List<string>>
+    private readonly Dictionary<string, List<string>> _subscriptions = new()
     {
-        { "F340FE8EA8604456AC4E66F31A87574C", [ "/catalogue" ] },
+        { "F340FE8EA8604456AC4E66F31A87574C", [ "/catalogue", "/inventory" ] },
         { "79AE4A5B04CC48B887E38FAE7D1282C0", [ "/search" ] },
-        { "7B6AD94DCC3C4E9F891C52C8C340D99E", [ "/user" ] }
+        { "7B6AD94DCC3C4E9F891C52C8C340D99E", [ "/user" ] },
+        { "7CDB41267D3D4DDEB847E64D6B3BE72B", ["/bff"] }
     };
 
     public Task<bool> CheckSubscriptionValidity(string subscriptionKey, string apiPath)
